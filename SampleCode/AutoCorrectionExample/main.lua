74b9f124936461f662ac93342524ff91c2f734f7
jcbnlsn <1qa2ws3ed>
2017-12-10 19:36:05 +0100

    update

--- SampleCode/AutoCorrectionExample/main.lua
+++ SampleCode/AutoCorrectionExample/main.lua
@@ -1,13 +1,12 @@
 ----------------------------------------------------------------------
 --
 --  Samplecode for keyboard events plugin 
---	Turn off auto-correction and spell checking example 
+--  Turn off auto-correction and spell checking example 
 --  Created by Jacob Nielsen (c) 2017
 --
 ----------------------------------------------------------------------
 
 local keyboardEvents = require "plugin.keyboardEvents"
-keyboardEvents.init()
 
 local textBox = native.newTextBox( display.contentCenterX, display.contentCenterY, display.contentWidth-40, 100 )
 textBox.placeholder = "Write some text..." 
